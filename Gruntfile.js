5604cc7666104b0fd99857e81619e4efbd97205b
Sylvain <sylvain@sgmultimedia.net>
2016-08-16 23:09:10 +0200

    Compression for SASS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 options: {
-                    style: 'compressed'
+                    outputStyle: 'compressed'
                 },
                 files: {
                     "web/assets/compiled/css/main.css": [
